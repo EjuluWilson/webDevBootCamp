f3d287e31d1c2fe93fd377a8699b5a46ba5fe5c5
Wilson Ejulu <emox1995@gmail.com>
2024-06-04 12:14:55 +0300

    finish basic express server

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+app.get("/",(req, res) =>{
+  res.send(`<h1> wilson Ejulu live at port ${port} </h1>`)
+})
+
+app.listen(port, () =>{
+  console.log(`This server is live at port:  ${port}`);
 });
