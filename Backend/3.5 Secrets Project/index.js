635fe788f29349c12ca7f76512c54f2dcf7b2c81
Wilson Ejulu <emox1995@gmail.com>
2024-06-04 13:55:40 +0300

    finish the secrets project

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,39 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const port  = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/check",(req, res)=>{
+    console.log(req.body.password);
+    let password = req.body.password;
+    if (password === "ejulu"){
+        res.sendFile(__dirname + "/public/secret.html");
+    }else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`I am listening at port ${port}`);
+})
+
+
+
+
+
+
