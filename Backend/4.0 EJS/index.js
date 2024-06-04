2e66620717611f3e884abc61d3d942e4c2ecf530
Wilson Ejulu <emox1995@gmail.com>
2024-06-04 15:52:43 +0300

    setup ejs

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
+
+// epress settings
+app.set("views", path.join(__dirname,"ejsfiles"));
+app.set("veiw engine", "ejs");
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
