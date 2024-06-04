b901d709d8928f4dd3e72980f8c59fd0cbbcf7c7
Wilson Ejulu <emox1995@gmail.com>
2024-06-04 16:16:22 +0300

    finisjh the weekday project

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 // epress settings
-app.set("views", path.join(__dirname,"ejsfiles"));
+app.set("views", path.join(__dirname, "ejsfiles"));
 app.set("veiw engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
-})
+const day = 0;
 
-app.listen(port,()=>{
-    console.log(`server live at port : ${port}`);
+let today = "weekday";
+let advice = "chase the bag";
 
-})
+app.get("/", (req, res) => {``
+
+  res.render("index.ejs", { adv: advice, date: today });
+});
+
+app.listen(port, () => {
+  console.log(`server live at port : ${port}`);
+});
