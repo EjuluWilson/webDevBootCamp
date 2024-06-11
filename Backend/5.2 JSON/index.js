d2d9e30b275a002f992e3be1d8e0fb840afa78ba
Wilson Ejulu <emox1995@gmail.com>
2024-06-11 11:27:37 +0300

    finish the taco town project

--- Backend/5.2 JSON/index.js
+++ Backend/5.2 JSON/index.js
@@ -10,15 +10,9 @@ const recipeJSON =
   '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
 const recipeList = JSON.parse(recipeJSON);
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+let choiceData; // recipeList item
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-let choiceData;
-
+// response object
 let choiceResponse = {
   recipename: undefined,
   proteinName: undefined,
@@ -26,6 +20,24 @@ let choiceResponse = {
   salsaName: undefined,
   toppingsList: undefined,
 };
+
+// returns a list of toppings from choiceData.toppings object list
+function listToppings(toppings) {
+  let toppingsList = [];
+  toppings.forEach((toppingObject) => {
+    toppingObject.ingredients.forEach((ingredient) => {
+      toppingsList.push(toppingObject.quantity + " of " + ingredient );
+    });
+  });
+  return toppingsList
+}
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
@@ -46,15 +58,13 @@ app.post("/recipe", (req, res) => {
   }
   choiceResponse.recipename = choiceData.name;
   choiceResponse.proteinName = choiceData.ingredients.protein.name;
-  choiceResponse.proteinPreparation = choiceData.ingredients.protein.preparation;
+  choiceResponse.proteinPreparation =
+    choiceData.ingredients.protein.preparation;
   choiceResponse.salsaName = choiceData.ingredients.salsa.name;
-  choiceResponse.toppingsList = choiceData.ingredients.toppings;
+  choiceResponse.toppingsList = listToppings(choiceData.ingredients.toppings);
 
-  res.render("index.ejs",choiceResponse);
+  res.render("index.ejs", choiceResponse);
 
-  console.log(`The recipe is: ${choiceResponse.recipename}`);
-  console.log(`The proteinName is: ${choiceResponse.proteinName}`);
-  console.log(`The salsaName is: ${choiceResponse.salsaName}`);
   console.log(`The toppingsList is: ${choiceResponse.toppingsList}`);
 });
 
