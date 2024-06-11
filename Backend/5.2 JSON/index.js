805d166c488b77156d3469cfd151140808bdedc3
Wilson Ejulu <emox1995@gmail.com>
2024-06-11 10:45:42 +0300

    renda ingredients

--- Backend/5.2 JSON/index.js
+++ Backend/5.2 JSON/index.js
@@ -8,6 +8,7 @@ const port = 3000;
 //Step 2: You can go to the recipe.json file to see the full structure of the recipeJSON below.
 const recipeJSON =
   '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+const recipeList = JSON.parse(recipeJSON);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,9 +17,43 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+let choiceData;
+
+let choiceResponse = {
+  recipename: undefined,
+  proteinName: undefined,
+  salsaName: undefined,
+  toppingsList: undefined,
+};
+
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+  const choice = req.body.choice;
+  switch (choice) {
+    case "chicken":
+      choiceData = recipeList[0];
+      break;
+    case "beef":
+      choiceData = recipeList[1];
+      break;
+    case "fish":
+      choiceData = recipeList[2];
+      break;
+    default:
+      break;
+  }
+  choiceResponse.recipename = choiceData.name;
+  choiceResponse.proteinName = choiceData.ingredients.protein.name;
+  choiceResponse.salsaName = choiceData.ingredients.salsa.name;
+  choiceResponse.toppingsList = choiceData.ingredients.toppings;
+
+  res.render("index.ejs",choiceResponse);
+
+  console.log(`The recipe is: ${choiceResponse.recipename}`);
+  console.log(`The proteinName is: ${choiceResponse.proteinName}`);
+  console.log(`The salsaName is: ${choiceResponse.salsaName}`);
+  console.log(`The toppingsList is: ${choiceResponse.toppingsList}`);
 });
 
 app.listen(port, () => {
