191d4d4f1f3a041c1a7e1a5e48ce07395e665690
Wilson Ejulu <emox1995@gmail.com>
2024-06-13 18:25:50 +0300

    use the all CRUD methods

--- Backend/5.5 REST APIs/index.js
+++ Backend/5.5 REST APIs/index.js
@@ -35,21 +35,67 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  try {
+    const data = {
+      secret: req.body.secret,
+      score: req.body.score,
+    };
+    const response = await axios.post(`${API_URL}/secrets`, data, {
+      headers: {
+        Authorization: `Bearer ${yourBearerToken}`,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: error.message });
+  }
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  try {
+    const data = {
+      secret: req.body.secret,
+      score: req.body.score,
+    };
+    const response = await axios.put(`${API_URL}/secrets/${searchId}`, data, {
+      headers: { Authorization: `Bearer ${yourBearerToken}` },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: error.message });
+  }
 });
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  try {
+    const data = {
+      secret: req.body.secret,
+      score: req.body.score,
+    };
+    const response = await axios.patch(`${API_URL}/secrets/${searchId}`, data, {
+      headers: { Authorization: `Bearer ${yourBearerToken}` },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: error.message });
+  }
 });
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+  try {
+    const response = await axios.delete(`${API_URL}/secrets/${searchId}`, {
+      headers: { Authorization: `Bearer ${yourBearerToken}` },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: error.message });
+  }
 });
 
 app.listen(port, () => {
